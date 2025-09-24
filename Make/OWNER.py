c263240b2aaab951cdf3faeece331f5cc05e074e
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2025-09-24 13:50:34 +0300

    Uploaded Successfully

---
+++ Make/OWNER.py
@@ -0,0 +1,22 @@
+# متطلبات التنصيب #
+
+#يوزر المطور
+OWNER = ["wwvvwl"] 
+OWNER__ID = 7728230165
+OWNER_DEVELOPER = 7728230165
+OWNER_NAME = "𓏺 َᴊᴀᴋلـ"
+#اسم التي سيظهر على صورة
+infophoto = "ᴊᴀᴋلـ"
+DATABASE = "mongodb+srv://huSeen96:Huseenslah96@cluster0.ld2v7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
+#قناة سورس
+CHANNEL = "https://t.me/xvwwvl"
+#كروب سورس
+GROUP = "https://t.me/xvwwvl"
+#فيديو سورس اذا ماعندك خليه
+VIDEO = "https://t.me/MusicxXxYousef/91"
+#صورة سورس اذا ماعندك خليها
+PHOTO = "https://t.me/MusicxXxYousef/90"
+#ساوي كروب وضيف عليه مصنع ورفعو اشراف وحط يوزر الكروب هنا
+LOGS = "xvwwvl"
+ 
+ 
